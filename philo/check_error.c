069edf441030a4b1c221dc37a5e656b990cee781
Dollyne Feret <doferet@paul-f4Ar3s2.clusters.42paris.fr>
2025-02-27 14:29:34 +0100

    philo plus que parfait

--- philo/check_error.c
+++ philo/check_error.c
@@ -6,7 +6,7 @@
 /*   By: doferet <doferet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 15:27:13 by doferet           #+#    #+#             */
-/*   Updated: 2025/02/21 12:34:29 by doferet          ###   ########.fr       */
+/*   Updated: 2025/02/27 14:15:22 by doferet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static bool	verif_minmax(char **av)
 		|| (ft_atol(av[5]) && ft_atol(av[5]) >= 2147483647)
 		|| ft_atol(av[5]) <= -2147483648)
 	{
-		write_error("Wrong input. Try with ex: ./philo 5 800 200 200");
+		write_error("Wrong input. Try with a valid value");
 		return (true);
 	}
 	return (false);
@@ -34,10 +34,9 @@ static bool	verif_minmax(char **av)
 
 bool	check_error(int ac, char **av)
 {
-	if (ac == 1 || ac < 5 || ac > 6)
+	if (ac < 5 || ac > 6)
 	{
-		write_error("Wrong input. Try with 5 or 6 arguments.\n"
-			"ex: ./philo 5 800 200 200");
+		write_error("Wrong input. Try with 5 or 6 arguments.");
 		return (true);
 	}
 	if (verif_minmax(av))
@@ -47,14 +46,14 @@ bool	check_error(int ac, char **av)
 		|| ft_atol(av[3]) <= 0 || !ft_isdigit(av[3])
 		|| ft_atol(av[4]) <= 0 || !ft_isdigit(av[4]))
 	{
-		write_error("Wrong input. Try with ex: ./philo 5 800 200 200");
+		write_error("Wrong input. Try with a valid value.");
 		return (true);
 	}
 	if (av[5])
 	{
 		if (ft_atol(av[5]) <= 0 || !ft_isdigit(av[5]))
 		{
-			write_error("Wrong input. Try with ex: ./philo 5 800 200 200");
+			write_error("Wrong input. Try with a valid value.");
 			return (true);
 		}
 	}
--- philo/think.c
+++ philo/think.c
@@ -6,7 +6,7 @@
 /*   By: doferet <doferet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 11:42:47 by doferet           #+#    #+#             */
-/*   Updated: 2025/02/27 12:06:15 by doferet          ###   ########.fr       */
+/*   Updated: 2025/02/27 14:26:20 by doferet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	philo_think(t_philo *philo)
 {
 	if (verif_death_full(philo, philo->mutex))
 		return ;
-	usleep(500);
+	if (philo->time_to_die != philo->time_to_eat + philo->time_to_sleep)
+		usleep(500);
 	safe_print("is thinking", philo, philo->mutex);
 }
